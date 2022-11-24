056c0f2ce27047131cd72a833ae0be8be81c548d
Zaharytto <zaxar.kravchenko.96@mail.ru>
2022-11-24 19:06:08 +0300

    technical task

--- TZ2/App/get.php
+++ TZ2/App/get.php
@@ -5,6 +5,9 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/TZ2/src/UserRepository.php';
 try {
     $userRepository = new UserRepository();
     $user = $userRepository->authorization($_POST['login'], $_POST['password']);
+    session_start();
+    setcookie('login', $_POST['login'], time() + 60 * 60 * 24 * 30, '/TZ2');
+    $_SESSION['login'] = $_POST['login'];
     
     echo json_encode([
         'status' => true,
